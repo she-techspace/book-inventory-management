ecb35b3b8318cc152468a3d2811eba74737e475c
SUE28 <SibongileMai@discovery.co.za>
2020-12-03 07:24:54 +0200

    intial - rest api module

---
+++ book-inventory-management/src/main/java/com/sibongile/techspace/bim/BookInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sibongile.techspace.bim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookInventoryManagementApplication.class, args);
+	}
+
+}
---
+++ book-inventory-management/src/test/java/com/sibongile/techspace/bim/BookInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sibongile.techspace.bim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
